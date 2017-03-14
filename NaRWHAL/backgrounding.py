860787b72084d89e21039eefb28f8f6cc1cd7e2f
Arushi Prakash <arushiprakash@Shrainiks-MacBook-Pro.local>
2017-03-13 19:45:45 -0700

    autopep8

--- NaRWHAL/backgrounding.py
+++ NaRWHAL/backgrounding.py
@@ -7,19 +7,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 %matplotlib inline
 
+
 def background_removal(source):
     """
     A function to remove gradients in the background of AFM micrographs. Takes in a .txt source file and returns the
     image as a numpy array with the background gradient removed.
     """
-    #Generate a numpy array from the .txt file and convert values from m to nm
+    # Generate a numpy array from the .txt file and convert values from m to nm
 
     height, width = afmdata.shape
 
-    #Remove any background that is greater than 3 stddevs from the mean. Removes gradients in the image.
+    # Remove any background that is greater than 3 stddevs from the mean.
+    # Removes gradients in the image.
     sigma_clip = SigmaClip(sigma=3., iters=10)
     bkg_estimator = MedianBackground()
-    bkg = Background2D(afmdata, (50, 50), filter_size=(3, 3), sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
+    bkg = Background2D(afmdata, (50, 50), filter_size=(
+        3, 3), sigma_clip=sigma_clip, bkg_estimator=bkg_estimator)
     backgrounded = afmdata - bkg.background
 
     return [backgrounded, bkg.background]
