bf66990f848dd55d53f498320fb2650c0ca3ee12
Arushi Prakash <arushiprakash@Shrainiks-MacBook-Pro.local>
2017-03-13 20:54:34 -0700

    final GUI changes

--- NaRWHAL/viewlims.py
+++ NaRWHAL/viewlims.py
@@ -29,45 +29,17 @@ def clear_canvas():
     if len(canvases) > 0:
         canvases[-1].get_tk_widget().delete("all")
         canvases[-1].get_tk_widget().destroy()
-
+        
+    if len(msgs) > 0:
+        msgs[-1].destroy()
+        msgs[-2].destroy()
     return
-
-
-def get_lims():
-	"""Getting size limits from user"""
-
-	master = tk.Tk()
-
-	msg = tk.Label(master, text="Enter limits").grid(row=0)
-
-	tk.Label(master, text="Min X [ nm ]").grid(row=1, column=0)
-	tk.Label(master, text="Max X [ nm ]").grid(row=1, column=2)
-	tk.Label(master, text="Min Y [ nm ]").grid(row=2, column=0)
-	tk.Label(master, text="Max Y [ nm ]").grid(row=2, column=2)
-
-	min_x = tk.Entry(master)
-	min_y = tk.Entry(master)
-	max_y = tk.Entry(master)
-	max_x = tk.Entry(master)
-
-	min_x.insert(10, "0")
-	min_y.insert(10, "0")
-	max_x.insert(10, "500")
-	max_y.insert(10, "500")
-
-	min_x.grid(row=1, column=1)
-	min_y.grid(row=2, column=1)
-	max_x.grid(row=1, column=3)
-	max_y.grid(row=2, column=3)
-
-	button = tk.Button(master, text="Dismiss", command=master.destroy).grid(row=3)
-
-	# return
 
 
 def upload_file():
     """ Upload the chosen file and plot the nanowire image onto the canvas"""
 
+    global filename
     filename = filedialog.askopenfilename()
     file_data = np.genfromtxt(filename)
 
@@ -75,7 +47,6 @@ def upload_file():
     file_data = file_data - np.min(file_data)
     file_data = file_data * (255.0) / np.max(file_data)
 
-    get_lims()
 
     afmimg, hull_points, wire_with_line, profile = ND.top_level(
         np.uint8(file_data))
@@ -88,17 +59,18 @@ def upload_file():
 
 
 def plot_data(md):
-	"""Plotting the 2-piece plot to show original image"""
-
+    """Plotting the 2-piece plot to show original image"""
+    	
     xmax, ymax = np.shape(md.data)
     Z = md(0, xmax, 0, ymax)
+    
     clear_canvas()
 
     plt.rcParams.update(params)
 
     fig1, (ax1, ax2) = plt.subplots(1, 2)
 
-    ax1.matshow(Z, origin='lower', extent=(0, 500, 0, 500), cmap=color_code, aspect='auto')
+    ax1.matshow(Z, origin='lower', extent=(0, xmax, 0, ymax), cmap=color_code, aspect='auto')
     ax2.matshow(Z, origin='lower', extent=(0, xmax, 0, ymax), cmap=color_code, aspect='auto')
 
     rect = UpdatingRect.UpdatingRect([0, 0], 0, 0, facecolor='None',
@@ -108,26 +80,41 @@ def plot_data(md):
 
     # Disable zooming into plot 1
     ax1.set_navigate(False)
+    ax1.set_title("Reference AFM Image")
+    ax1.xaxis.set_tick_params(labeltop='off', labelbottom='on')
 
     ax2.callbacks.connect('xlim_changed', rect)
     ax2.callbacks.connect('ylim_changed', rect)
     ax2.callbacks.connect('xlim_changed', md.ax_update)
     ax2.callbacks.connect('ylim_changed', md.ax_update)
-    ax2.set_title("Zoom here")
+    ax2.xaxis.set_tick_params(labeltop='off', labelbottom='on')
+    ax2.set_title("Zoom into a single nanowire here")
 
     # Create canvas and toolbar
     canvas = FigureCanvasTkAgg(fig1, top)
     toolbar = CustomToolbar.CustomToolbar(canvas, top)
+    msg1 = tk.Message(top, text="Showing file:\n"+filename, width=800, aspect=1000, background='#f7fcb9')
 
     # Book keeping
     canvases.append(canvas)
     toolbars.append(toolbar)
 
     # Packing the toolbar and plot into the canvas
-    toolbar.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    msg1.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+    toolbar.pack(side=tk.TOP, fill=tk.X,anchor=tk.CENTER)
     canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
     canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
-
+    
+    msg2 = tk.Message(top, width=700, aspect=1000, 
+    	background='#f7fcb9', text="Zoom into your preferred nanowire on the right image \
+    	or reset (Home symbol). When you've selected a region with a single nanowire,\
+    	initialize analysis by Image Processing->Background slope removal. You can also \
+    	choose your favorite color scheme from above (Image Processing->Color).")
+    msg2.pack(side=tk.BOTTOM, fill=tk.BOTH, expand=1)
+	
+    msgs.append(msg1)
+    msgs.append(msg2)
+	
     return
 
 def nano_analyze(data):
@@ -136,30 +123,48 @@ def nano_analyze(data):
 	afmimg, hull_points, wire_with_line, profile = ND.top_level(np.uint8(data[::-1]))
 	fig, (ax1,ax2, ax3) = plt.subplots(1, 3)
 	ax1.matshow(wire_with_line, cmap=color_code, aspect='auto')
-	ax2.matshow(afmimg, cmap=color_code, aspect='auto')
-	ax2.scatter(hull_points[:,0],hull_points[:,1], c='white')
-	ax3.plot(profile)
+	ax1.set_title("Line along cross-section of wire")
+	ax1.xaxis.set_tick_params(labeltop='off', labelbottom='on')
+	
+	ax3.matshow(afmimg, cmap=color_code, aspect='auto')
+	ax3.xaxis.set_tick_params(labeltop='off', labelbottom='on')
+	ax3.scatter(hull_points[:,0],hull_points[:,1], c='white')
+	
+	ax2.plot(profile)
+	ax2.set_title("Height profile along the line")
+	ax2.xaxis.set_tick_params(labeltop='off', labelbottom='on')
+	
 	# Create canvas and toolbar
 	top = tk.Toplevel()
 	top.geometry('{}x{}'.format(800, 500))
 	top.title("Nanowire detection results")
 	canvas = FigureCanvasTkAgg(fig, top)
+	msg = tk.Message(top, text="The algorithm detects single nanowires in the \
+	given snippet. It makes a line perpendicular to the wire and calculates the height\
+	 of points along the line. The algoirthm also detects and outline of the nanowire",
+	  width=800, aspect=1000, background='#f7fcb9')
 	
 	# Packing the toolbar and plot into the canvas
+	msg.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 	canvas._tkcanvas.pack(side=tk.TOP, fill=tk.BOTH, expand=1)
 	canvas.get_tk_widget().pack(side=tk.TOP, fill=tk.BOTH, expand=1)
+	
 	return
 
 def remove_background():
 	"""Detecting and removing gradient background (only) from image"""
 
 	backgrounded, background = backgroundremoval.backgroundremoval(np.uint8(md.subset))
-	
+    
 	top = tk.Toplevel()
 	top.title("Backgrounding results")
 	top.configure(background='#f7fcb9')
 	top.geometry('{}x{}'.format(800, 500))
 
+	msg = tk.Message(top, width=800, aspect=1000, background='#f7fcb9', 
+	text="The algorithm removes percetible slopes from the background of the image")
+	msg.pack(side=tk.TOP, fill=tk.X, expand=1)
+	
 	button1 = tk.Button(top, text="Dismiss Changes", command=top.destroy)
 	button1.pack()
 	button2 = tk.Button(top, text="Accept Background and Analyze", command=lambda: nano_analyze(backgrounded))
@@ -168,9 +173,11 @@ def remove_background():
 	plt.rcParams.update(params)
 	fig, (ax1,ax2) = plt.subplots(1, 2)
 	ax1.matshow(backgrounded, origin='lower', cmap=color_code, aspect='auto')
-	ax1.set_title("Backgrounded image")
+	ax1.set_title("Image after processing")
+	ax1.xaxis.set_tick_params(labeltop='off', labelbottom='on')
 	ax2.matshow(background, origin='lower', cmap=color_code, aspect='auto')
-	ax2.set_title("Background of image")
+	ax2.xaxis.set_tick_params(labeltop='off', labelbottom='on')
+	ax2.set_title("Background noise removed from image")
 	
 	# Create canvas and toolbar
 	canvas = FigureCanvasTkAgg(fig, top)
@@ -183,7 +190,7 @@ def remove_background():
 
 
 def recolor(color_selection):
-	"""Recolor the plots according to user selection"""
+    """Recolor the plots according to user selection"""
 
     global color_code
     color_code = color_selection
@@ -195,7 +202,7 @@ def recolor(color_selection):
 
 
 def open_readme():
-	"""Open user documentation"""
+    """Open user documentation"""
 	
     window = tk.Toplevel()
     window.title('Documentation')
@@ -210,7 +217,7 @@ def open_readme():
 
 params = {
         'axes.labelsize': 12,
-        'font.size': 12,
+        'font.size': 10,
         'legend.fontsize': 12,
         'xtick.labelsize': 12,
         'ytick.labelsize': 12,
@@ -224,7 +231,7 @@ params = {
 
 # FOR BOOK KEEPING
 # Maintaining all the labels here
-labels = []
+msgs = []
 # Maintaining all the canvases here
 canvases = []
 # Maintaining all the toolbars here
@@ -232,9 +239,8 @@ toolbars = []
 # Input file
 md = 0
 color_code = 'viridis'
-# Ranges
-xmin, xmax=0.0,0.0
-ymin, ymax=0.0,0.0
+filename=''
+
 
 
 # CONFIGURING THE GUI OBJECT
