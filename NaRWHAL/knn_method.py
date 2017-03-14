2b4a1239d7e72ebe7d1286f32300f25f36382563
Arushi Prakash <arushiprakash@Shrainiks-MacBook-Pro.local>
2017-03-13 19:46:59 -0700

    autopep8

--- NaRWHAL/knn_method.py
+++ NaRWHAL/knn_method.py
@@ -3,42 +3,42 @@ import math
 import matplotlib.pyplot as plt
 
 wire_image = np.loadtxt('100-0_72_NW-003')
-#side = np.linspace(1,len(wire_image),len(wire_image))                                                                                                                                                     
-side = np.linspace(1,40,40)
-x,y = np.meshgrid(side,side)
+#side = np.linspace(1,len(wire_image),len(wire_image))
+side = np.linspace(1, 40, 40)
+x, y = np.meshgrid(side, side)
 plt.figure(1)
-plt.contourf(x,y,wire_image[321:361,321:361],cmap='bwr')
+plt.contourf(x, y, wire_image[321:361, 321:361], cmap='bwr')
 plt.colorbar()
 plt.show()
 
-single_wire = wire_image[321:361,321:361]
+single_wire = wire_image[321:361, 321:361]
 
-for j in range(0,len(side)):
-        for jj in range(0,len(side)):
-                if single_wire[j,jj] < 0:
-                        single_wire[j,jj] = 0
-                else:
-                        pass
+for j in range(0, len(side)):
+    for jj in range(0, len(side)):
+        if single_wire[j, jj] < 0:
+            single_wire[j, jj] = 0
+        else:
+            pass
 
-count = np.zeros((len(side),len(side)))
+count = np.zeros((len(side), len(side)))
 top = np.max(single_wire)
 bottom = np.min(single_wire)
 
-for j in range(0,len(side)):
-        for jj in range(0,len(side)):
-                dist_from_top = top - single_wire[j,jj]
-                dist_from_bottom = single_wire[j,jj] - bottom
-                if dist_from_top < dist_from_bottom:
-                        count[j,jj] = 1
-                else:
-                        count[j,jj] = 0
+for j in range(0, len(side)):
+    for jj in range(0, len(side)):
+        dist_from_top = top - single_wire[j, jj]
+        dist_from_bottom = single_wire[j, jj] - bottom
+        if dist_from_top < dist_from_bottom:
+            count[j, jj] = 1
+        else:
+            count[j, jj] = 0
 
 plt.figure(2)
-plt.subplot(1,2,1)
-plt.contourf(x,y,single_wire,cmap='bwr')
+plt.subplot(1, 2, 1)
+plt.contourf(x, y, single_wire, cmap='bwr')
 plt.colorbar()
-plt.subplot(1,2,2)
-plt.contourf(x,y,count,cmap='bwr',levels=[0,0.5,1])
+plt.subplot(1, 2, 2)
+plt.contourf(x, y, count, cmap='bwr', levels=[0, 0.5, 1])
 plt.colorbar()
 plt.tight_layout()
 plt.show()
